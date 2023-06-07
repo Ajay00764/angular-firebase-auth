a1af7deb873968e4e0825cf0aa06740a4742b977
Ajay <kharatajay314@gmail.com>
2023-06-07 17:49:07 +0530

    firebase installed

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-firebase-auth-524d2',
+    appId: '1:440756983231:web:844e024d0219e301207d0a',
+    storageBucket: 'angular-firebase-auth-524d2.appspot.com',
+    apiKey: 'AIzaSyD4qkduahibj2eFbLE8nFP5xNEk1QiHHcc',
+    authDomain: 'angular-firebase-auth-524d2.firebaseapp.com',
+    messagingSenderId: '440756983231',
+  },
+};
